27f78d43f6a708c6903ccc40e2af2f5e7c2b116f
Kapioga <ldaleyvo@gmail.com>
2023-10-25 17:32:07 -0400

    Added Gopher to scene

--- Scripts/vegetables.gd
+++ Scripts/vegetables.gd
@@ -1,6 +1,7 @@
-extends CharacterBody2D
+extends StaticBody2D
 
 @onready var player_p = get_node("res://Scenes/Player.tscn")
+@onready var veggie = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +11,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
