da2ae376f118e0d917ae84fabf2b409e18e0ee2f
Kapioga <ldaleyvo@gmail.com>
2023-10-25 21:30:02 -0400

    Fixed node control issues

--- Scenes/start_screen.gd
+++ Scenes/start_screen.gd
@@ -1,8 +1,25 @@
 extends Control
 
-func _on_quit_button_pressed():
-	get_tree().quit()
+#Variables
+@onready var animation_player = $Buttons/PlayButton/AnimationPlayer
 
+
+#PLAY Button
+func _on_play_button_mouse_entered():
+	animation_player.play("MenuWiggle")
+
+func _on_play_button_mouse_exited():
+	animation_player.stop()
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Game_Main.tscn")
+
+#QUIT Button
+func _on_quit_button_mouse_exited():
+	animation_player.stop()
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+func _on_quit_button_mouse_entered():
+	animation_player.play("QuitButWiggle")
--- Scripts/Game_Main.gd
+++ Scripts/Game_Main.gd
@@ -8,6 +8,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	leavepan()
 
-func
+#Exits to splash screen
+func leavepan():
+	if Input.is_action_just_pressed("Exit"):
+		get_tree().change_scene_to_file("res://Scenes/start_screen.tscn")
