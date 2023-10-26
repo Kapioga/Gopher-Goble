632b43fea7f9cf060f80f55c74e570bf33d43198
Kapioga <ldaleyvo@gmail.com>
2023-10-26 14:57:01 -0400

    Added score system (mock)

--- Scripts/Game_Main.gd
+++ Scripts/Game_Main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var label = $ScoreLabel
+var score: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +16,12 @@ func _process(delta):
 func leavepan():
 	if Input.is_action_just_pressed("Exit"):
 		get_tree().change_scene_to_file("res://Scenes/start_screen.tscn")
+
+
+
+#Adds score to label
+func _on_score_detect_area_entered(area):
+	var newscore = 1
+	score += newscore
+	label.text = str(score)
+
