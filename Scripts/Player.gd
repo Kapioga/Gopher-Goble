9fd596cb8497b340f33bfaeffe65387d788f0392
Kapioga <ldaleyvo@gmail.com>
2023-10-26 18:05:25 -0400

    Updated Player and Veggie nodes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 @onready var collision_shape_2d = $CollisionShape2D
+@onready var veggie = $"../Veggie"
+@onready var player = $"."
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var grabbed = false
 
 func _physics_process(delta):
 	gravity_check(delta)
@@ -42,3 +44,13 @@ func crouch():
 
 func Emitgrab():
 	pass
+
+
+func _on_score_detect_veggie_entered(area):
+	var grabbed = true
+	var grab_hover = player.global_position
+	if grabbed == true:
+		veggie.global_position = grab_hover
+	if Input.is_action_just_pressed("Space") and grabbed == true:
+		grabbed = false
+	#print("Touch!")
--- Scripts/vegetables.gd
+++ Scripts/vegetables.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends RigidBody2D
 
 #@onready var player_p = get_node("res://Scenes/Player.tscn")
 #@onready var veggie = $"."
